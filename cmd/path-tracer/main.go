867dc15f3519eb552b3ddb559df0fc8a9f6da9ce
Mukesh <hey.mukesh272921@gmail.com>
2024-12-25 23:21:07 +0530

    ...

--- cmd/path-tracer/main.go
+++ cmd/path-tracer/main.go
@@ -12,11 +12,11 @@ func main() {
 	imageWidth := 1000
 	imageHeight := int(float64(imageWidth) / aspectRatio)
 
-	lookFrom := pkg.NewVector(0.2, 0.01, 1.5)
-	lookAt := pkg.NewVector(0, 0.5, -1)
+	lookFrom := pkg.NewVector(1, 2, 1)
+	lookAt := pkg.NewVector(0, 0, -1)
 	vUp := pkg.NewVector(0, 1, 0)
 
-	camera := pkg.NewCamera(lookFrom, lookAt, vUp, 40, float64(imageWidth)/float64(imageHeight))
+	camera := pkg.NewCamera(lookFrom, lookAt, vUp, 30, float64(imageWidth)/float64(imageHeight))
 
 	f, err := os.Create("test.ppm")
 	if err != nil {
@@ -28,21 +28,24 @@ func main() {
 		panic(err.Error())
 	}
 
-	ns := 200
+	ns := 300
 
 	materialGround := pkg.NewLambertianMaterial(pkg.NewColor(0.8, 0.8, 0))
-	materialCentre := pkg.NewLambertianMaterial(pkg.NewColor(0.1, 0.2, 0.5))
-	materialLeft := pkg.NewDielectricMaterial(1.5)
+	materialLambertian := pkg.NewLambertianMaterial(pkg.NewColor(0.1, 0.2, 0.5))
+	materialGlass := pkg.NewDielectricMaterial(1.5)
 	materialBubble := pkg.NewDielectricMaterial(1.0 / 1.5)
-	materialRight := pkg.NewMetalMaterial(pkg.NewColor(0.8, 0.6, 0.2), 0)
+	materialMetal := pkg.NewMetalMaterial(pkg.NewColor(0.8, 0.6, 0.2), 0)
 
 	ground := pkg.NewSphere(pkg.NewVector(0, -100.5, -1), 100, materialGround)
-	centre := pkg.NewSphere(pkg.NewVector(0, 0, -1.2), 0.5, materialCentre)
-	left := pkg.NewSphere(pkg.NewVector(-1, 0, -1), 0.5, materialLeft)
+	centre := pkg.NewSphere(pkg.NewVector(0, 0, -1.2), 0.5, materialLambertian)
+	left := pkg.NewSphere(pkg.NewVector(-1, 0, -1), 0.5, materialGlass)
 	bubble := pkg.NewSphere(pkg.NewVector(-1, 0, -1), 0.4, materialBubble)
-	right := pkg.NewSphere(pkg.NewVector(1, 0, -1), 0.5, materialRight)
+	right := pkg.NewSphere(pkg.NewVector(1, 0, -1), 0.5, materialMetal)
+	topLeft := pkg.NewSphere(pkg.NewVector(-1, 1, -1), 0.5, materialGlass)
+	topMiddle := pkg.NewSphere(pkg.NewVector(0, 1, -1), 0.5, materialMetal)
+	topRight := pkg.NewSphere(pkg.NewVector(1, 1, -1), 0.5, materialGlass)
 
-	scene := pkg.Scene{Elements: []pkg.Hittable{&ground, &centre, &left, &bubble, &right}}
+	scene := pkg.Scene{Elements: []pkg.Hittable{&ground, &centre, &left, &bubble, &right, &topLeft, &topMiddle, &topRight}}
 
 	camera.Render(scene, imageHeight, imageWidth, ns, f)
 }
